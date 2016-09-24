39cb420a2b1a879fff89851be3389cc532618c17
Sagang Wee <sagangwee@yahoo.com>
2016-09-24 14:16:02 -0700

    creating shots 06

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody>();
+		// get z component
+		rb.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void pdate () {
+	
+	}
+}
