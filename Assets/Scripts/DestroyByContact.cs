4d68e8f76a5d00690ad48981f1baa66729882ba5
Sagang Wee <sagangwee@yahoo.com>
2016-09-24 15:05:50 -0700

    creating hazards 09

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "boundary") {
+			return;
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
